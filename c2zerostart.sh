8628e83267f50666ca77e0ef421e305dae976977
Tim Panton <tim@pi.pe>
2023-02-09 18:09:19 +0100

    Added pdf made script work added local show added srtplight dep

--- c2zerostart.sh
+++ c2zerostart.sh
@@ -18,7 +18,9 @@ v4l2-ctl -d /dev/video0 \
 
 v4l2-ctl -c saturation=100
 
-cd /home/pi
-JAVA_HOME=/home/pi/zulu11.62.17-ca-jdk11.0.18-linux_aarch32hf export JAVA_HOME
+#uncomment for a local azul jdk
+#JAVA_HOME=/home/pi/zulu11.62.17-ca-jdk11.0.18-linux_aarch32hf export JAVA_HOME
+#PATH=${PATH}:${JAVA_HOME}/bin export PATH
 
-${JAVA_HOME}/bin/java -Xmx128m -cp target/clank2-1.0-SNAPSHOT.jar pe.pi.clank2.Clank2 224 224 3 /dev/null  
+
+java -Xmx128m -cp target/clank2-1.0-SNAPSHOT.jar:lib/srtplight-1.1.10.jar pe.pi.clank2.Clank2 224 224 3 /dev/null  
