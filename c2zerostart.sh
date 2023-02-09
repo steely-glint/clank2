8e511f329c44698d20e70f4cc6ab9e78a9b47d3e
Tim Panton <tim@pi.pe>
2023-02-09 18:17:43 +0100

    bullseye camera

--- c2zerostart.sh
+++ c2zerostart.sh
@@ -13,14 +13,17 @@ sleep 1
 (echo 1 > /sys/class/pwm/pwmchip0/pwm0/enable)
 (echo 1 > /sys/class/pwm/pwmchip0/pwm1/enable)
 
-v4l2-ctl -d /dev/video0 \
+#bullseye camera config
+# older raspios uses /dev/video0
+CAMERADEV=/dev/video12 export CAMERADEV
+v4l2-ctl -d ${CAMERADEV} \
  --set-fmt-video width=224,height=224,pixelformat=2 -p 30
 
-v4l2-ctl -c saturation=100
+v4l2-ctl -c saturation=100 -d ${CAMERADEV}
 
 #uncomment for a local azul jdk
 #JAVA_HOME=/home/pi/zulu11.62.17-ca-jdk11.0.18-linux_aarch32hf export JAVA_HOME
 #PATH=${PATH}:${JAVA_HOME}/bin export PATH
 
 
-java -Xmx128m -cp target/clank2-1.0-SNAPSHOT.jar:lib/srtplight-1.1.10.jar pe.pi.clank2.Clank2 /dev/video0 224 224 3 /dev/null  
+java -Xmx128m -cp target/clank2-1.0-SNAPSHOT.jar:lib/srtplight-1.1.10.jar pe.pi.clank2.Clank2 ${CAMERADEV} 224 224 3 /dev/null  
